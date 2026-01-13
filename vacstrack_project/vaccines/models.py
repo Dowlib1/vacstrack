77981dfe578304b73729019b033eeb5dd15eeeed
Dowlib1 <oladimeji0107@gmail.com>
2026-01-14 00:06:23 +0100

    add vaccinelog: in models.py

--- vacstrack_project/vaccines/models.py
+++ vacstrack_project/vaccines/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class VaccineLog(models.Model):
+    VACCINE_TYPES = [
+        ('BCG', 'BCG'),
+        ('HepB', 'Hepatitis B'),
+        ('OPV', 'Oral Polio Vaccine'),
+        ('DPT', 'Diphtheria, Pertussis, Tetanus'),
+        ('Hib', 'Haemophilus influenzae type b'),
+        ('PCV', 'Pneumococcal Conjugate'),
+        ('Rotavirus', 'Rotavirus'),
+        ('Measles', 'Measles / MMR'),
+        ('Yellow Fever', 'Yellow Fever'),
+        ('COVID-19', 'COVID-19'),
+        ('Flu', 'Influenza'),
+        ('HPV', 'Human Papillomavirus'),
+        ('Other', 'Other'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vaccine_type = models.CharField(max_length=50, choices=VACCINE_TYPES)
+    date_administered = models.DateField()
+    next_due_date = models.DateField(null=True, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.vaccine_type} - {self.date_administered}" # Or  = f"{self.vaccine_type} administered on {self.date_administered}
