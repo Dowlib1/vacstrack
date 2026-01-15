d807bd4cee9b60addeb7c57dee5d3b110f651616
Dowlib1 <oladimeji0107@gmail.com>
2026-01-16 00:44:18 +0100

    Fix: safe admin registration for VaccineLog

--- vacstrack_project/vaccines/admin.py
+++ vacstrack_project/vaccines/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered
 from .models import VaccineLog
 
-admin.site.register(VaccineLog)
-
-@admin.register(VaccineLog)
 class VaccineLogAdmin(admin.ModelAdmin):
     list_display = ('user', 'vaccine_type', 'date_administered', 'next_due_date')
     list_filter = ('vaccine_type',)
     search_fields = ('user__username', 'notes')
+
+# Register safely: if already registered, unregister then register with our admin
+try:
+    admin.site.register(VaccineLog, VaccineLogAdmin)
+except AlreadyRegistered:
+    admin.site.unregister(VaccineLog)
+    admin.site.register(VaccineLog, VaccineLogAdmin)
